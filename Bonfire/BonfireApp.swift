eecd1762074025930fccc390efa28cf5f7ba0986
Cristian Rojas <cristian.rojas@live.fr>
2023-12-02 11:56:13 +0100

    Initial Commit

---
+++ Bonfire/BonfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonfireApp.swift
+//  Bonfire
+//
+//  Created by Cristian Felipe Patiño Rojas on 02/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BonfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
